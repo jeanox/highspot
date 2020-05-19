6601d26598b59c37f0b83417a752f25b79da4e9e
biki <beckypennock@gmail.com>
2020-05-19 12:11:51 -0600

    Updates

---
+++ precache-manifest.490effa6d84c174ee407d2ca6e119aad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77d34ae7a18be2683fa7ee76db4ef92",
+    "url": "/react-infinite-cards/index.html"
+  },
+  {
+    "revision": "fe4018ffd5967cb4d480",
+    "url": "/react-infinite-cards/static/css/main.32d10994.chunk.css"
+  },
+  {
+    "revision": "e9d0e1e3701b15ad4978",
+    "url": "/react-infinite-cards/static/js/2.4d1649fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-infinite-cards/static/js/2.4d1649fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4018ffd5967cb4d480",
+    "url": "/react-infinite-cards/static/js/main.74e4e74d.chunk.js"
+  },
+  {
+    "revision": "d343856c2ff20b5e6588",
+    "url": "/react-infinite-cards/static/js/runtime-main.8ae957dc.js"
+  },
+  {
+    "revision": "469a66b5254631fabf4f79e554d13ca9",
+    "url": "/react-infinite-cards/static/media/load.469a66b5.gif"
+  }
+]);
